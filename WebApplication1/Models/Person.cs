b1af78a85690945085b2b92b094bdb6efa093c82
Alex <fernandezalex7@yahoo.com>
2023-12-18 16:45:40 -0500

    Add project files.

---
+++ WebApplication1/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Race { get; set; }
+
+        public string Gender { get; set; }
+
+        public Person()
+        {
+
+        }
+    }
+}
